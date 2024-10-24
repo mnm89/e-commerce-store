a6c3278d7c3b212e13a3bf785f31269f3d0ba505
Mohamed mabrouk <mohamed.mabrouk@flexperto.com>
2024-10-24 20:23:24 +0200

    dockerize backoffice

--- backoffice/medusa-config.ts
+++ backoffice/medusa-config.ts
@@ -1,5 +1,4 @@
 import { loadEnv, defineConfig } from "@medusajs/framework/utils";
-import { RedisLockingProvider } from "@medusajs/locking-redis/dist/services/redis-lock";
 
 loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
@@ -7,12 +6,24 @@ module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
+      storeCors: process.env.STORE_CORS || "http://localhost:8000",
+      adminCors: process.env.ADMIN_CORS || "http://localhost:9000",
+      authCors: process.env.AUTH_CORS || "http://localhost:9000",
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
+    workerMode: (process.env.WORKER_MODE as any) || "shared",
+    redisUrl: process.env.REDIS_URL,
+    sessionOptions: {
+      resave: false,
+      saveUninitialized: false,
+      ttl: 1000 * 60 * 60 * 24,
+    },
+  },
+  admin: {
+    disable: process.env.DISABLE_ADMIN === "true",
+    backendUrl: process.env.BACKEND_URL || "http://localhost:9000",
+    path: (process.env.ADMIN_PATH as any) || "/app",
   },
   modules: [
     {
@@ -25,6 +36,14 @@ module.exports = defineConfig({
       resolve: "@medusajs/medusa/cache-redis",
       options: {
         redisUrl: process.env.REDIS_URL,
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/workflow-engine-redis",
+      options: {
+        redis: {
+          url: process.env.REDIS_URL,
+        },
       },
     },
     {
