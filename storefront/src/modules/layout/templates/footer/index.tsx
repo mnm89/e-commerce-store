bdd02a705926176bf0d6c32ee696cc11e2cc3565
Mohamed mabrouk <mohamed.mabrouk@flexperto.com>
2024-10-24 21:09:36 +0200

    remove unused code

--- storefront/src/modules/layout/templates/footer/index.tsx
+++ storefront/src/modules/layout/templates/footer/index.tsx
@@ -1,14 +1,8 @@
-import { getCategoriesList } from "@lib/data/categories"
-import { getCollectionsList } from "@lib/data/collections"
-import { Text, clx } from "@medusajs/ui"
+import { Text } from "@medusajs/ui"
 
-import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import MedusaCTA from "@modules/layout/components/medusa-cta"
 
 export default async function Footer() {
-  const { collections } = await getCollectionsList(0, 6)
-  const { product_categories } = await getCategoriesList(0, 6)
-
   return (
     <footer className="border-t border-ui-border-base w-full">
       <div className="flex w-full my-3 justify-between text-ui-fg-muted content-container">
